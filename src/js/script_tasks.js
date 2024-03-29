0391a4ce0f1208af31c49526b381bea80ab61331
jakhongir <jakhongir@jakhongirs-MBP.home>
2024-03-28 22:41:35 -0700

    task-2

---
+++ src/js/script_tasks.js
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use strict';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// const a = prompt('Сколько фильмов вы уже посмотрели?', ""),
+//       b = prompt('На сколько оцените его?'),
+//       c = prompt('Сколько фильмов вы уже посмотрели?', ""),
+//       d = prompt('На сколько оцените его?');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+
+//task_2 started here
+for (let i = 0; i<2; i++){
+    const a = prompt('Сколько фильмов вы уже посмотрели?', ""),
+          b = prompt('На сколько оцените его?');
+//#2
+    if( a != null && b != null && a !='' && b !='' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }
+    else{
+        console.log('error');
+        i--;
+    }
+}
+//#3
+    if(personalMovieDB.count <10){
+        console.log('Просмотрено довольно мало фильмов');
+    }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log('Вы классический зритель');
+    }else if (personalMovieDB.count >= 30){
+        console.log('Вы киноман');
+    }else{
+        console.log('Произошла ошибка');
+    }
+
+
+console.log(personalMovieDB);
