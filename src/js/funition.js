66a557957ca4accc0eba690e696da64c1ae82cce
jahon <danaboev.jf@gmail.com>
2024-04-08 06:46:46 +0500

    edu_methods

---
+++ src/js/funition.js
@@ -0,0 +1,27 @@
+//////доп урок про функции а пргументы
+// const usdCurr = 28;
+// const eurCurr = 30;
+
+
+// function convert(amount, curr){
+//     console.log(curr * amount);
+// }
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+////доп урок про return
+
+const usdCurr = 28;
+const discount = 0.8
+
+function convert(amount, curr){
+    return curr * amount;
+}
+
+function promotion(result){
+    console.log (result * discount);
+}
+
+promotion(convert(500, usdCurr));
+
+
---
+++ src/js/metods.js
@@ -0,0 +1,40 @@
+"use strict";
+///methods - методы и свойства строк и чисел
+
+const str = 'test';
+// const arr = ['1,2,3'];
+// console.log(str.length);
+// console.log(arr.length);
+// console.log(str[2]);
+///method
+console.log(str.toUpperCase());
+
+///поиск по строку
+
+const fruit = 'some fruit';
+console.log(fruit.indexOf("fruit"));
+
+//slice() 
+// substring() 
+// substr() 
+// 3-случая 
+
+const logg = "hello world";
+console.log(logg.slice(6, 10));
+// console.log(logg.slice(6));
+console.log(logg.slice(-6, -1)); //с обратной стороной
+console.log(logg.substring(6, 8));
+console.log(logg.substr(6, 5));
+
+//for numbers
+
+const num = 12.3;
+console.log(Math.round(num));
+
+const tes = '12.3px'
+console.log(parseInt(tes));
+console.log(parseFloat(tes));
+
+
+
+
