a0dc71f32a1c7f06b228eb49edfe31195b6de795
jahon <danaboev.jf@gmail.com>
2024-04-16 05:03:59 +0500

    variables

---
+++ src/js/edu/varliable.js
@@ -0,0 +1,41 @@
+"use strict" /* new version */
+
+let number = 5;
+const leftBorder =1; 
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+}
+obj.a = 10
+console.log(obj)
+// old version
+console.log(name)
+var name = 'jahon';
+
+{
+    let result =50;
+    var result =50;
+
+}
+console.log(result);
+/* must have ; */      
+alert(5);
+[].push('a');
+
+// naming var with rules
+
+const widthBody = 100;
+const hightBody = 200;
+
+console.log("Ширина автомобиля:" + widthBody + 'высота авто:' + hightBody);
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+let COLOR_RED = '#f00';
+const _apiBase = 'https://ecxmake.com';
