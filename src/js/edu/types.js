f21115188700f025eac75eade48a85434dbbda07
jahon <danaboev.jf@gmail.com>
2024-04-16 05:41:25 +0500

    types

---
+++ src/js/edu/types.js
@@ -0,0 +1,34 @@
+'use strict';
+//num types
+let num = 4 ;
+let num2 = 4.4 ;
+
+console.log(4/0); /* infinity */
+console.log('string' * 9); /* NaN */
+
+//string
+const person = 'alex';
+const numStr = '5';
+
+//boolean true or false
+const bool = true;
+
+//null
+console.log(somthing);
+
+//undefined
+let und; 
+console.log(und)
+
+//object-complex
+const obj = {
+    name: 'john',
+    age:27,
+    isMarried: false
+};
+console.log(obj.name);
+console.log(obj["name"]);
+
+//array
+let arr = ["img1", "img2", 5, "img3",{},[]];
+console.log(arr[2]);
