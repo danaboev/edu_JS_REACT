b70c3d77a07fad478a053bbaa6cefa5196d4e5ee
jahon <danaboev.jf@gmail.com>
2024-04-16 06:19:59 +0500

    array va object

--- src/js/edu/types.js
+++ src/js/edu/types.js
@@ -32,3 +32,55 @@ console.log(obj["name"]);
 //array
 let arr = ["img1", "img2", 5, "img3",{},[]];
 console.log(arr[2]);
+
+//ARRAY vs OBJECT
+//syntax
+const array = ['1', '2', '3'];
+console.log(array[1]);
+
+array[10] = '12345' //dont recomeded
+console.log(array[10]);
+console.log(array);
+
+
+
+
+const object = { a: 1 , b: 2, c: 3};
+
+const workers = {
+    'anna' : 500,
+    'ben' :600,
+    alex:700
+}
+//functional
+const arrObj = {
+    a: 'a',
+    '1': 'b',
+    2: 'c'
+};
+const b = 'b';
+console.log(arrObj.a);
+console.log(arrObj[1]);
+console.log(arrObj[2]);
+// adding value
+// arrObj.b = '123';
+// arrObj['b'] = '1234';
+arrObj[b] = '1234';
+
+console.log(arrObj['b']); /* adding  new string value */
+console.log(arrObj.b); /* calling b */
+
+// obj in obj,array
+const arrObj2 = {
+    a: 'a',
+    '1': 'b',
+    2: 'c',
+    abc:{
+        b:[],
+        d:{}
+    }
+    
+};
+arrObj2[b] = {};
+arrObj2[b] = [];
+
