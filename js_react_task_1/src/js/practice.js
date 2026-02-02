8a05ee0df0b342052757f5bd235d2c01d224a77e
jahon <danaboev.jf@gmail.com>
2026-02-02 21:54:06 +0500

    practice part 1

--- js_react_task_1/src/js/practice.js
+++ js_react_task_1/src/js/practice.js
@@ -2,27 +2,28 @@
 // 'use strict';
 // // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-//task_1
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
-
-// // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies:{},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+//practice_part_1 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ' ');
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// const a = prompt('Сколько фильмов вы уже посмотрели?', ""),
-//       b = prompt('На сколько оцените его?'),
-//       c = prompt('Сколько фильмов вы уже посмотрели?', ""),
-//       d = prompt('На сколько оцените его?');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const a = prompt('Сколько фильмов вы уже посмотрели?', ""),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Сколько фильмов вы уже посмотрели?', ""),
+      d = prompt('На сколько оцените его?');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
 
 //task_2 started here
 // for (let i = 0; i<2; i++){
