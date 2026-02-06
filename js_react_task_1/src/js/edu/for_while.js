a351e7b0ce7ee4877e551ac611824e87dd8091b2
jahon <danaboev.jf@gmail.com>
2026-02-06 07:51:14 +0500

    task break and continue

--- js_react_task_1/src/js/edu/for_while.js
+++ js_react_task_1/src/js/edu/for_while.js
@@ -32,3 +32,36 @@ for(let i = 0; i < 10; i++){
 	}
 	console.log(i);
 }
+
+//task for in for 
+for(let i = 0; i < 3; i++){
+	console.log(i);
+	for(let j = 0; j < 3; j++){
+		console.log(j);
+	}
+}
+// task ***
+let result = '';
+const length = 7;
+
+for(let i = 1; i < length; i++){
+	for(let j = 0; j < i; j++){
+		result += '*';
+	}
+	result += '\n';
+}
+console.log(result);
+///task continue and break 
+
+first: for(let i = 0; i < 3; i++){
+	console.log(`first level ${i}`);
+	for(let j = 0; j < 3; j++){
+		console.log(`second level ${j}`);
+		for(let k = 0; k < 3; k++){
+			// if(k === 2) continue first;
+			if(k === 2) break first;
+
+		console.log(`third level ${k}`);
+		}
+	}
+}
