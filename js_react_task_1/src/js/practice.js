5e6f9c0299bae564d41706d3ff8c4ae4970717b4
jahon <danaboev.jf@gmail.com>
2026-02-26 10:07:42 +0500

    practice

--- js_react_task_1/src/js/practice.js
+++ js_react_task_1/src/js/practice.js
@@ -1,54 +1,54 @@
-// /* eslint-disable @typescript-eslint/no-unused-vars */
-// 'use strict';
-// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use strict';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 //practice_part_1 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ' ');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ' ');
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies:{},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies:{},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const a = prompt('Сколько фильмов вы уже посмотрели?', ""),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Сколько фильмов вы уже посмотрели?', ""),
-      d = prompt('На сколько оцените его?');
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// const a = prompt('Сколько фильмов вы уже посмотрели?', ""),
+//       b = prompt('На сколько оцените его?'),
+//       c = prompt('Сколько фильмов вы уже посмотрели?', ""),
+//       d = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 //task_2 started here
-// for (let i = 0; i<2; i++){
-//     const a = prompt('Сколько фильмов вы уже посмотрели?', ""),
-//           b = prompt('На сколько оцените его?');
-// //#2
-//     if( a != null && b != null && a !='' && b !='' && a.length < 50){
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//     }
-//     else{
-//         console.log('error');
-//         i--;
-//     }
-// }
+for (let i = 0; i<2; i++){
+    const a = prompt('Сколько фильмов вы уже посмотрели?', ""),
+          b = prompt('На сколько оцените его?');
+//#2
+    if( a != null && b != null && a !='' && b !='' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }
+    else{
+        console.log('error');
+        i--;
+    }
+}
 //#3
-//     if(personalMovieDB.count <10){
-//         console.log('Просмотрено довольно мало фильмов');
-//     }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-//         console.log('Вы классический зритель');
-//     }else if (personalMovieDB.count >= 30){
-//         console.log('Вы киноман');
-//     }else{
-//         console.log('Произошла ошибка');
-//     }
-// console.log(personalMovieDB);
+    if(personalMovieDB.count <10){
+        console.log('Просмотрено довольно мало фильмов');
+    }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log('Вы классический зритель');
+    }else if (personalMovieDB.count >= 30){
+        console.log('Вы киноман');
+    }else{
+        console.log('Произошла ошибка');
+    }
+console.log(personalMovieDB);
 
 
